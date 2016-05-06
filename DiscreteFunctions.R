7461306a415488e2fe83dacbd9dcec84a841837a
Ryan Roney <Ryan Roney>
2016-05-06 13:38:10 -0400

    plodding along

--- DiscreteFunctions.R
+++ DiscreteFunctions.R
@@ -4,8 +4,11 @@ library(corHMM)
 library(phytools)
 library(phangorn)
 
-?pace#You can use code you wrote for the correlation exercise here.
+?pace #You can use code you wrote for the correlation exercise here.
 
+discrete.data <- rTraitDisc(tree, model = "ER", k=2, states=c(0,1))
+
+continuous.data <- rTraitCont(tree, model="BM")
 
 VisualizeData <- function(phy, data) {
 	#Important here is to LOOK at your data before running it. Any weird values? Does it all make sense? What about your tree? Polytomies?
@@ -13,4 +16,5 @@ VisualizeData <- function(phy, data) {
 
 CleanData <- function(phy, data) {
 	#treedata() in Geiger is probably my favorite function in R.
+  treedata(phy, data, sort=FALSE, warnings=TRUE)
 }
